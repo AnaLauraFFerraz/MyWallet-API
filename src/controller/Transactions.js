2326cfcc51bd1add4260cfd23d04bbed804cb2ac
AnaLauraFFerraz <analaurafferraz@gmail.com>
2023-01-22 13:59:37 -0300

    list transactions for get transactions request

---
+++ src/controller/Transactions.js
@@ -0,0 +1,16 @@
+import { ObjectId } from "mongodb"
+import db from "../config/database"
+
+export async function listTransactions(req, res) {
+    const userSession = await db.collection("users").findOne({ _id: session.userId })
+    
+    if (!userSession) return res.status(401).send("Sem autorização")
+
+    delete userSession.password
+
+    res.send(userSession)
+}
+
+export async function newIncome(req, res) {}
+
+export async function newExpense(req, res) {}
