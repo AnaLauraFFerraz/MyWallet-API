772024d155e50604c88eed6f97f1efb5c56e4e91
AnaLauraFFerraz <analaurafferraz@gmail.com>
2023-01-22 13:01:06 -0300

    server config

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from "express"
+import cors from "cors"
+import authRouter from "./routes/AuthRoutes"
+import transactionsRouter from "./routes/TransactionsRoutes"
+
+const server = express()
+server.use(express.json())
+server.use(cors())
+
+const PORT = 5000
+
+server.listen(PORT, () => {
+    console.log(`Server listening port ${PORT}`)
+})
